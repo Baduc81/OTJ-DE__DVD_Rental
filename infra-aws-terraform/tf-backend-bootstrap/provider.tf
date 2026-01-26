10ebb6704da29f9d80f0c8033e05efad1b65d869
Phan Ba Duc <baduc81@gmail.com>
2026-01-26 18:40:44 +0700

    feat/migrate state file to remote

--- infra-aws-terraform/tf-backend-bootstrap/provider.tf
+++ infra-aws-terraform/tf-backend-bootstrap/provider.tf
@@ -1,10 +1,17 @@
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.28.0"
+    backend "s3" {
+        bucket       = "dvd-rental-ducpb7-tuantd24"
+        key          = "tf_backend/terraform.tfstate"
+        region       = "us-east-1"
+        encrypt      = true
+        use_lockfile = true
     }
-  }
+    required_providers {
+        aws = {
+            source = "hashicorp/aws"
+            version = "6.28.0"
+        }
+    }
 }
 
 provider "aws" {
