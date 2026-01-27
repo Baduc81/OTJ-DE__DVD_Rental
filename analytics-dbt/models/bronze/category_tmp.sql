6a19a1143494b29712f8841ab87e324df053e085
Tuan Tran <tuantran2803@gmail.com>
2026-01-27 02:46:11 +0000

    feat: category_temporary model create

--- analytics-dbt/models/bronze/category_tmp.sql
+++ analytics-dbt/models/bronze/category_tmp.sql
@@ -1,24 +1,24 @@
 {%- set standard_cols = [
-    'category_name',
-    'valid_from',
-    'file_name'
+    "category_name",
+    "valid_from",
+    "file_name"
 ] -%}
 
-{%- set unique_key = 'category_id' -%}
-{%- set join_table = 'dvd_rental.silver.category' -%}
-{%- set source = source('stream','category') -%}
+{%- set unique_key = "category_id" -%}
+{%- set join_table = "dvd_rental.silver.category" -%}
+{%- set source = source("bronze","category_strm") -%}
 
 WITH add_total_occurrneces_col_cte as (
     {{ add_total_occurrneces_col(source, standard_cols, unique_key) }}
 ),
 filter_dup_data_within_file_cte as (
-    {{ filter_dup_data_within_file('add_total_occurrneces_col_cte') }}
+    {{ filter_dup_data_within_file("add_total_occurrneces_col_cte") }}
 ),
 add_md5_hash_col_cte as (
-    {{ add_md5_hash_col('filter_dup_data_within_file_cte', standard_cols, unique_key) }}
+    {{ add_md5_hash_col("filter_dup_data_within_file_cte", standard_cols, unique_key) }}
 ),
 filter_dup_data_version_cte as (
-    {{ filter_dup_data_version('add_md5_hash_col_cte', join_table, unique_key, standard_cols) }}
+    {{ filter_dup_data_version("add_md5_hash_col_cte", join_table, unique_key, standard_cols) }}
 )
 
 SELECT
