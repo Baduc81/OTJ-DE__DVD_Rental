c427ebc596cfc166d82993175172decdc11a9b32
Phan Ba Duc <baduc81@gmail.com>
2026-01-28 13:20:00 +0700

    feat/create role, policy in AWS and storage integration in snowflake

--- infra-aws-terraform/dvd-rental-infra/locals.tf
+++ infra-aws-terraform/dvd-rental-infra/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  local_data_path = "/home/luminous/OTJ FPT/dvdrental"
+  local_data_path = "/home/luminous/OTJ FPT/dvdrental/data_csv"
   csv_files       = fileset(local.local_data_path, "*.csv")
 }
