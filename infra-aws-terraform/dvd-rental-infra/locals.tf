c1dd2da2d5259784bd5edad793999948c4965db1
Phan Ba Duc <baduc81@gmail.com>
2026-02-05 11:15:22 +0700

    chore/grant permission to role github action terraform policy

--- infra-aws-terraform/dvd-rental-infra/locals.tf
+++ infra-aws-terraform/dvd-rental-infra/locals.tf
@@ -1,5 +1,6 @@
 locals {
   local_data_path = "/home/luminous/OTJ FPT/dvdrental/data_csv"
+  # csv_files       = fileset(local.local_data_path, "*.csv")
   csv_files = [
     "actor.csv",
     "address.csv",
